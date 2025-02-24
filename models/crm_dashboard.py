ce8a68b6eade58a28a38f696a0fbbb70691d90b5
James Oginga <otipapajim@gmail.com>
2025-02-24 17:39:01 +0300

    Fixed a wrong import

--- models/crm_dashboard.py
+++ models/crm_dashboard.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 class OdooCrmAnalytics(models.Model):
     _name = 'crm.dashboard'
     _description = 'Odoo CRM Analytics'
-    _inherit=['mail','mail.thread', 'mail.mixin']
+    _inherit=['mail.thread', 'mail.mixin']
 
 
     name = fields.Char(string='Name', required=True, tracking=True)
